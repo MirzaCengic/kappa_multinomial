199cf540b67cec764d68bf948591d314e8511ec0
Will Cornwell <w.k.cornwell@vu.nl>
2015-10-06 21:23:16 +1100

    moving things around a bit

--- R/kappa_multinomial.R
+++ R/kappa_multinomial.R
@@ -1,3 +1,37 @@
+##' Multinomial models are difficult to assess in across--model way.  
+##' 
+##' @title Performance statistics for multinomial models
+##' 
+##' @encoding utf8
+##' 
+##' @param i input, probably should generalize this
+##' 
+##' @param input figure out logical structure for this
+##' 
+##' @param ...  Additional arguments passed to ?
+##' 
+##' @return returns a list with the following elements: 
+##'  \itemize{
+##' \item{Kappa}
+##' \item{some other kappa}
+##' }
+##' 
+##' @export
+##' 
+##' @details This is details
+##' @author Bob Douma
+##' 
+##' @references 
+##' NULL
+##' @seealso 
+##' NULL
+##' 
+##' @examples 
+##' 
+##' NULL
+##' 
+
+
 # kappa calculation
 kappa_multinomial<-function(i,input,...){
   obs_total<-input[[i]]$observed                                        
