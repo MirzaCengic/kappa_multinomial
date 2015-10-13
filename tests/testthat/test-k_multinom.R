d14314607bc5c5493bd779cf222c49ec6ecc2458
Will Cornwell <w.k.cornwell@vu.nl>
2015-10-13 21:13:31 +1100

    testing all three functions

--- tests/testthat/test-k_multinom.R
+++ tests/testthat/test-k_multinom.R
@@ -2,11 +2,15 @@ context("")
 
 test_that("calculations sane", {
   pred <- gtools::rdirichlet(100, c(0.1,0.1,0.5,0.5)) # generate multinomial probabilties with four classes
-  obs <- t(apply(pred,1,rmultinom,size=1,n=1))
-  k_stats = kappa_multinomial(obs=obs,pred=pred)
-  expect_that(k_stats,is_a("list"))
-  expect_true(k_stats$pmax<1 & k_stats$pmax>0)
-  expect_true(k_stats$k_prob<1 & k_stats$k_prob>0)
+  obs <- t(apply(pred, 1, rmultinom, size=1, n=1))
+  k_stats <- kappa_multinomial(obs=obs, pred=pred)
+  expect_that(k_stats, is_a("list"))
+  expect_true(k_stats$pmax<1 & k_stats$pmax > 0)
+  expect_true(k_stats$k_prob<1 & k_stats$k_prob > 0)
+  auc <- AUC_multiclass(obs=obs, pred=pred)
+  expect_true(auc$AUC.mean>0)
+  ck <- cohen_kappa(obs=obs, pred=pred)
+  expect_true(ck$po>0)
 })
 
 
