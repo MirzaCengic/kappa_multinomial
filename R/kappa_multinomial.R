a1951772ef86d16d43b9307a55a19157d0a6d974
bobdouma <bob.douma@wur.nl>
2016-03-31 14:56:48 +0200

    helper function added

--- R/kappa_multinomial.R
+++ R/kappa_multinomial.R
@@ -19,6 +19,22 @@
 ##' 
 ##' @export kappa_multinomial_stats
 ##' 
+##' # function to prepare data.frame for kappa_multinomial calculation
+
+kappa_multinomial_stats = function(obs, pred,...){
+  obs.agg.dat = obs
+  pred.agg.dat = pred
+  observed<-apply(obs.agg.dat,2,sum,na.rm=TRUE) 
+  # calculates marginal totals
+  predicted<-apply(pred.agg.dat,2,sum,na.rm=TRUE)
+  common.freq<-(1-(apply(abs(obs.agg.dat-pred.agg.dat),1,sum)/2)) #calculates the fraction of cells in common between pred and obs      
+  realized<-sum(common.freq,na.rm=TRUE)                           #number of cells in common on total raster
+  pmax = mean(apply(pred.agg.dat,1,max))
+  return(list(observed=observed, predicted=predicted,realized=realized,pmax=pmax))  
+}
+
+
+##' 
 ##' @details This is details
 ##' @author Bob Douma
 ##' 
