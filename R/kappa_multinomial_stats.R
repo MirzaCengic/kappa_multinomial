1ee26ccbb0acbc82718539e02ff5ba8913f71a2b
bobdouma <bob.douma@wur.nl>
2016-04-04 11:16:22 +0200

    test

--- R/kappa_multinomial_stats.R
+++ R/kappa_multinomial_stats.R
@@ -4,6 +4,7 @@ kappa_multinomial_stats = function(obs, pred,...){
   abs.diff = abs(obs - pred)
   po = sum(1-apply(abs.diff,1,sum)/2)/nrow(obs)
   pmax = mean(apply(pred,1,max))
-  return(c("po"=po,"pmax"=pmax))  
+  pmax.test = mean(1-(apply(pred,1,max)- apply(obs,1,max)))
+  return(c("po"=po,"pmax"=pmax,"pmax.test"=pmax.test))  
 }
 
