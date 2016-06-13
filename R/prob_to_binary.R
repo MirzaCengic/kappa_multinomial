c44c754e0b075299e0a842168549d42331ebeb68
bobdouma <bob.douma@wur.nl>
2016-06-13 14:56:44 +0200

    add helper functions

--- R/prob_to_binary.R
+++ R/prob_to_binary.R
@@ -1,3 +1,32 @@
+##'   
+##' @title prob_to_binary
+##' 
+##' @description This function transform probabilties to presence absence, by assuming that the class with highest sample probability if present and the others classes are absent
+##' 
+##' 
+##' @param pred a data.frame with class predictions n columns and m rows; each rows represents a sample, the columns represent the classes of outcomes. 
+##' 
+##' @return returns a list with the following elements: 
+##' \item{pred}
+##' 
+##' @export 
+##' 
+##' @details helper function for the tutorial
+##' @author Bob Douma
+##' 
+##' @references 
+##' NULL
+##' @seealso 
+##' NULL
+##' 
+##' @examples 
+##' library(gtools)
+##' pred = rdirichlet(100, c(0.1,0.1,0.5,0.5)) # generate multinomial probabilties with four classes
+##' pred.bin = prob_to_binary(pred)
+##' 
+##' 
+##' 
+
 # This function apply the function select.max to a data.frame consisting of multiple rows.
 prob_to_binary = function(pred){
 pred.type = t(apply(pred,1,select_max)) 
--- R/select_max.R
+++ R/select_max.R
@@ -1,3 +1,31 @@
+##'   
+##' @title select maximum
+##' 
+##' @description This function select the class with highest probability in a sample. If multiple classes have the same heighest probability then one is randomly chosen
+##' 
+##' 
+##' @param x a vector with sample predictions: the elements represent the classes of outcomes. 
+##' 
+##' @return returns a vector with the following elements: 
+##' \item{y}
+##' 
+##' @export 
+##' 
+##' @details helper function for the tutorial
+##' @author Bob Douma
+##' 
+##' @references 
+##' NULL
+##' @seealso 
+##' NULL
+##' 
+##' @examples 
+##' library(gtools)
+##' pred = rdirichlet(100, c(0.1,0.1,0.5,0.5)) # generate multinomial probabilties with four classes
+##' pred.bin = prob_to_binary(pred)
+##' 
+##' 
+##' 
 # this function converts multinomial probability distribution to presence/absence. If multiple classes are predicted with the same maximum probability one class is chosen.
 
 select_max = function(x){
