e2b73dd0288b29eb0ebabbe11ee1616eb3320c03
wcornwell <wcornwell@gmail.com>
2016-10-03 21:34:14 +0200

    passing cran checks

--- R/kappa_multinomial.R
+++ R/kappa_multinomial.R
@@ -28,19 +28,19 @@
 ##' NULL
 ##' 
 ##' @examples 
-##' library(gtools)
-##' pred = rdirichlet(100, c(0.1,0.1,0.5,0.5)) # generate multinomial probabilties with four classes
-##' obs = t(apply(pred,1,rmultinom,size=1,n=1)) # generate multinomial observations with four classes
-##' kappa_multinomial(obs=obs,pred=pred) # calculate kappa multinomial
-##' 
-##' 
-##' 
+##' # generate multinomial probabilties with four classes
+##' pred = data.frame(gtools::rdirichlet(100, c(0.1,0.1,0.5,0.5)))
+##' # generate multinomial observations with four classes 
+##' obs = data.frame(t(apply(pred,1,rmultinom,size=1,n=1)))
+##' # calculate kappa multinomial 
+##' kappa_multinomial(obs=obs,pred=pred) 
 ##' 
 
+
 # kappa calculation
-kappa_multinomial<-function(obs,pred,...){
+kappa_multinomial<-function(obs, pred){
   # do checks
-  if (!is.data.frame(obs)){stop("observations not in a dataframe")}  
+  if (!is.data.frame(obs)&!is.matrix(obs)){stop("observations not in a dataframe")}  
   if (!(dim(pred)[1] == dim(obs)[1] & dim(pred)[2] == dim(obs)[2])){stop("data.frames of unequal size")}
   if (sum(apply(pred,1,sum)) != nrow(pred)){stop("rowsums of predictions not equal to one")}
   if (sum(apply(obs,1,sum)) != nrow(obs)){stop("rowsums of observations not equal to one")}
@@ -52,14 +52,14 @@ kappa_multinomial<-function(obs,pred,...){
   k_prob<-(po-pe)/(pmax-pe) # Eq. 6
   k_loc<-(pmax-pe)/(1-pe) # Eq. 6
   k_multinomial <- k_loc*k_prob # # Eq. 6
-  out = c(k_prob=k_prob,k_loc=k_loc,k_multinomial=k_multinomial,po = po,pe=pe,pmax=pmax)
-  names(out) = c("k_prob","k_loc","k_multi","po","pe","pmax") 
+  out = data.frame(k_prob=k_prob,k_loc=k_loc,k_multinomial=k_multinomial,po = po,pe=pe,pmax=pmax)
+  #names(out) = c("k_prob","k_loc","k_multi","po","pe","pmax") 
   return(out) #returns kappa values
 }
 
 
 # function to prepare data.frame for kappa_multinomial calculation
-kappa_multinomial_stats = function(obs, pred,...){
+kappa_multinomial_stats = function(obs, pred){
   abs.diff = abs(obs - pred)
   po = sum(1-apply(abs.diff,1,sum)/2)/nrow(obs) # eq. 7 
   pmax = sum(1-apply(abs(t(apply(pred,1,sort))-t(apply(obs,1,sort))),1,sum)/2)/nrow(pred) # Equation 8 in the paper (it is called new because compared to the previous version of the paper; it should replace pmax when you agree on the method)
